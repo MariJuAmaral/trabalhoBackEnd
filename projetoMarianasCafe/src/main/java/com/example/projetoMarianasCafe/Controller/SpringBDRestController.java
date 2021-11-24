152c394ff7d3919971734418125b77dfbc6540c1
Mariana <71901221@aluno.faculdadecotemig.br>
2021-11-23 22:30:37 -0300

    versão final, rest controller

--- projetoMarianasCafe/src/main/java/com/example/projetoMarianasCafe/Controller/SpringBDRestController.java
+++ projetoMarianasCafe/src/main/java/com/example/projetoMarianasCafe/Controller/SpringBDRestController.java
@@ -2,15 +2,10 @@ package com.example.projetoMarianasCafe.Controller;
 
 import com.example.projetoMarianasCafe.Model.ItemCarrinho;
 import com.example.projetoMarianasCafe.Model.Produto;
-import com.example.projetoMarianasCafe.Service.CarrinhoService;
-import com.example.projetoMarianasCafe.Service.CompraService;
 import com.example.projetoMarianasCafe.Service.ItemCarrinhoService;
 import com.example.projetoMarianasCafe.Service.ProdutoService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -28,12 +23,34 @@ public class SpringBDRestController {
         return produtoService1.getAllProdutos();
     }
 
+    //itemCarrinho
     @RequestMapping(value = "/rest/insertItemCarrinho", method = RequestMethod.POST)
     public void updateAluno(@RequestBody ItemCarrinho item) {
         itemCarrinhoService.insertItemCarrinho(item);
     }
+
     @RequestMapping(value = "/rest/getAllItensCarrinho", method = RequestMethod.GET)
     public List<ItemCarrinho> getAllItens() {
         return itemCarrinhoService.getAllItensCarrinho();
+    }
+
+    @RequestMapping(value = "/rest/deleteAll", method = RequestMethod.DELETE)
+    public void deleteItens() {
+        itemCarrinhoService.deleteAllItensCarrinho();
+    }
+
+    @RequestMapping(value = "/rest/delete/{id}", method = RequestMethod.DELETE)
+    public void deleteItens(@PathVariable("id") Integer id) {
+        itemCarrinhoService.deleteItemCarrinhoById(id);
+    }
+
+    @RequestMapping(value = "/rest/update/{id}", method = RequestMethod.POST)
+    public void updateItens(@RequestBody ItemCarrinho itemCarrinho, @PathVariable("id") Integer id) {
+        itemCarrinhoService.updateItemCarrinhoById(id, itemCarrinho);
+    }
+
+    @RequestMapping(value = "/rest/insert", method = RequestMethod.POST)
+    public void updateItens(@RequestBody ItemCarrinho itemCarrinho) {
+        itemCarrinhoService.insertItemCarrinho(itemCarrinho);
     }
 }
